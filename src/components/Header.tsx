935861d1d89262363e49efbe154768a5ec3893d4
x0beR-143n <pkjoker69@gmail.com>
2024-11-05 23:30:15 +0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,12 @@ import React from "react";
 import { Lobster } from 'next/font/google'
 import { Kanit } from 'next/font/google'
 import {Divider} from "@nextui-org/divider";
-import {  Dropdown,  DropdownTrigger,  DropdownMenu,  DropdownSection,  DropdownItem} from "@nextui-org/dropdown";
+import {  Dropdown,  DropdownTrigger,  DropdownMenu,  DropdownItem} from "@nextui-org/dropdown";
 import { IoMenu } from "react-icons/io5";
 import {Button} from "@nextui-org/button";
 import { useRouter } from "next/navigation";
+import Link from 'next/link';
+
 const lobster = Lobster({
     subsets: ['latin'],
     weight: ['400'],
@@ -27,7 +29,11 @@ const Header: React.FC = () => {
     return (
         <header className="border-b border-slate-300 text-green-950 py-4">
             <div className="w-10/12 m-auto flex items-center justify-between">
-                <h1 className={`${lobster.className} sm:text-5xl 2sm:text-2xl mr-9`} id="header_name"><a href="/">DREAM LABOUR</a></h1>
+            <h1 className={`${lobster.className} sm:text-5xl 2sm:text-2xl mr-9`}>
+                <Link href="/">
+                    <a>DREAM LABOUR</a>
+                </Link>
+            </h1>
                 <div className={`1100:block 2sm:hidden`}>
                     <div className={`${kanit.className} lg:text-lg sm:text-sm font-normal flex h-5 items-center space-x-4 justify-between gap-x-2`}>
                         <a href="/services" className="no-underline hover:underline">Services</a>
