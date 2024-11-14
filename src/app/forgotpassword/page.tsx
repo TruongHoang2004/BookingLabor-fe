239e63eb6cf70f5f3d04fe3756c9fad5c0aad792
hwimae <dnimer211@gmail.com>
2024-11-14 10:47:26 +0700

    fixed forgot and reset password

--- src/app/forgotpassword/page.tsx
+++ src/app/forgotpassword/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ForgotPasswordForm from '../../components/ForgotPasswordForm';
+import ForgotPasswordForm from '../../components/authenticate/ForgotPasswordForm';
 
 const ForgotPasswordPage = () => {
     return (
