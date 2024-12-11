90572aec35c3384e0f879dc0fcc525fee6ae723e
Hung Le <lequanghungsergioaguero@gmail.com>
2024-12-12 01:00:04 +0700

    fix vercel

--- src/components/review/RatingandReviewTasker.tsx
+++ src/components/review/RatingandReviewTasker.tsx
@@ -52,9 +52,11 @@ export default function RatingandReviewTasker() {
                     border: '2px solid #ccc',
 
                 }}>
-                    <Image 
+                    <Image
                         src="https://b.fssta.com/uploads/application/soccer/headshots/885.vresize.350.350.medium.19.png"
                         alt="Tasker"
+                        width={80}
+                        height={80}
                         style={{
                             width: '100%',
                             height: 'auto',
