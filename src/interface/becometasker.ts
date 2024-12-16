aac2535981c9a8b01d408c9c79964ee21ae8f64d
Hung Le <lequanghungsergioaguero@gmail.com>
2024-12-16 17:12:21 +0700

    call api become tasker

---
+++ src/interface/becometasker.ts
@@ -0,0 +1,25 @@
+import { User } from './user';
+import { Skill } from './user';
+
+export interface TaskerForm {
+    skillIds: number[];
+    work_area: number[];
+    experience: string;
+}
+
+export interface TaskerResponse {
+
+    work_area: string;
+    exprerience: string;
+    user: User
+    skill: Skill[]
+    create_at: string;
+    update_at: string;
+    id: number;
+    completed_tasks: number;
+    avg_rating: number;
+    rating_count: number;
+
+
+}
+
